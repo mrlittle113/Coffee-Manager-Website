43d3b68a23c2317d36af320f35a94ef5e5300e78
longnguyen98 <60703822+longnguyen98@users.noreply.github.com>
2020-07-15 18:48:19 +0700

    15072020 18:48

--- WebApplication1/Controllers/StaffController.cs
+++ WebApplication1/Controllers/StaffController.cs
@@ -17,5 +17,9 @@ namespace WebApplication1.Controllers
         {
             return View();
         }
+        public ActionResult Info()
+        {
+            return View();
+        }
     }
 }
